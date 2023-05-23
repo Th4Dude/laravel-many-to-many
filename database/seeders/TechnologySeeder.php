b3acc69bcae780be9da3b8da2fe94af92b40a26d
Th4Dude <gallottimaurizio@yahoo.it>
2023-05-23 17:27:05 +0300

    scaffoling + migration + seeder

---
+++ database/seeders/TechnologySeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Technology;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
+
+class TechnologySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $technologies = ['JS', 'Laravel', 'Html', 'CSS', 'VueJS', 'Sass'];
+
+        /* Schema::disableForeignKeyConstraints(); */
+        Technology::truncate();
+        /* Schema::enableForeignKeyConstraints(); */
+        foreach ($technologies as $technology) {
+            $newTechnology = new Technology();
+            $newTechnology->name = $technology;
+            $newTechnology->slug = Str::slug($newTechnology->name);
+            $newTechnology->save();
+        }
+    }
+}
