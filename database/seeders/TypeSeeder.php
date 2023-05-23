2d3d90103153986dbf03e51903cf973a2a061ca9
Maurizio Gallotti <117756194+Th4Dude@users.noreply.github.com>
2023-05-23 16:52:02 +0300

    Initial commit

---
+++ database/seeders/TypeSeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Type;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Str;
+
+class TypeSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $types = ['Frontend', 'Backend', 'AI', 'DBA'];
+
+        Schema::disableForeignKeyConstraints();
+        Type::truncate();
+        Schema::enableForeignKeyConstraints();
+        foreach ($types as $type) {
+            $newType = new Type();
+            $newType->name = $type;
+            $newType->slug = Str::slug($newType->name);
+            $newType->save();
+        }
+    }
+}
